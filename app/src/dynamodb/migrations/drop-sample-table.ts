bcf3ada71ed98f8d813d281c144f1ee860d5165a
teddybear0430 <teddybear0430@hotmail.com>
2021-08-09 20:43:53 +0900

    feat: 不要なので消した。

--- app/src/dynamodb/migrations/drop-sample-table.ts
+++
@@ -1,4 +0,0 @@
-import { dbFunc } from '../database';
-
-const { deleteTable } = dbFunc();
-deleteTable('sample_table').catch(er => console.error(JSON.stringify(er, null, 2)));
--- app/src/utils/create-time.ts
+++
@@ -1,9 +0,0 @@
-export const createTime = () => {
-  const dt = new Date();
-  const year = dt.getFullYear();
-  const date = dt.getDate();
-  const hours = dt.getHours();
-  const minutes = dt.getMinutes();
-
-  return `${year}-${date}-${hours}:${minutes}`;
-};
