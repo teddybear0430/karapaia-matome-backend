b470175fe6dcf459ad45361196cd6c773a5918ec
teddybear0430 <teddybear0430@hotmail.com>
2021-08-15 00:20:13 +0900

    feat: リントの設定微調整。

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  env: {
+    es6: true,
+    node: true,
+  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
 };
