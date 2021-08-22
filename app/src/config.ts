cb7e82c586081d0bc9f7d730468cf79051c5b234
teddybear0430 <teddybear0430@hotmail.com>
2021-08-22 12:21:33 +0900

    feat: テーブル名を環境に応じて切り替えられるようにして。

--- app/src/config.ts
+++ app/src/config.ts
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const appConfig = {
-  tableName: 'karapaia_matome',
+  tableName: process.env.isDevelopment ? 'karapaia_matome_dev' : process.env.TABLE_NAME as string,
   url: 'https://karapaia.com/',
   pageNum: 5,
 } as const;
